1be6b79a22a4faabb9516e9707ca7a30a20ff4de
Alireza Safaierad <frontendmonster@gmail.com>
2025-01-10 17:24:58 +0100

    debug

--- src/errors/serverError.ts
+++
@@ -1,9 +0,0 @@
-export default class ServerError {
-  public status: number;
-  public message: string;
-
-  constructor(status: number, message: string) {
-    this.message = message;
-    this.status = status;
-  }
-}
